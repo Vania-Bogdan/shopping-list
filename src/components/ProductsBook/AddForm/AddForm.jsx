e71fb3e3a7aec02b45349fc80a4483fc0a172921
Vania Bogdan <bogdan.vania@gmail.com>
2023-10-03 08:23:10 +0200

    new design

--- src/components/ProductsBook/AddForm/AddForm.jsx
+++ src/components/ProductsBook/AddForm/AddForm.jsx
@@ -1,57 +1,59 @@
-import React from "react";
+import React from 'react';
 
 class AddForm extends React.Component {
-    state = {
-        name: '',
-        number: '1'
-    }
-    onInputChange = e => {
-        const { name, value } = e.currentTarget;
-        this.setState({ [name]: value });
-    }
-    onSubmitForm = e => {
-        e.preventDefault();
-        this.props.onSubmit(this.state);
-        this.clearInput()
-    };
-    clearInput = () => {
-        this.setState({ name: "", number: '1' });
-    }
+  state = {
+    name: '',
+    number: '1',
+  };
+  onInputChange = e => {
+    const { name, value } = e.currentTarget;
+    this.setState({ [name]: value });
+  };
+  onSubmitForm = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state);
+    this.clearInput();
+  };
+  clearInput = () => {
+    this.setState({ name: '', number: '1' });
+  };
 
-    render() {
-        return (
-            <form className="add-form" onSubmit={this.onSubmitForm}>
-                <label className="form-label">
-                <h3 className="form-heading">Продукт</h3>
-                <input
-                    className="form-input"
-                    type="text"
-                    autoComplete="off"
-                    name="name"
-                    title="Enter product name."
-                    required
-                    value={this.state.name}
-                    onChange={this.onInputChange}
-                />
-                </label>
-                <label className="form-label">
-                    <h3 className="form-heading">Кількість</h3>
-                    <input
-                    className="form-input-num"
-                    type="number"
-                    autoComplete="off"
-                    name="number"
-                    min="1"
-                    pattern="^[0-9]+$"
-                    title="Enter products quantity."
-                    value={this.state.number}
-                    onChange={this.onInputChange}
-                    />
-                </label>
-                <button className="form-btn" type="submit">Додати</button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form className="add-form" onSubmit={this.onSubmitForm}>
+        <label className="form-label">
+          <h3 className="form-heading">Name</h3>
+          <input
+            className="form-input"
+            type="text"
+            autoComplete="off"
+            name="name"
+            title="Enter product name."
+            required
+            value={this.state.name}
+            onChange={this.onInputChange}
+          />
+        </label>
+        <label className="form-label">
+          <h3 className="form-heading">Number</h3>
+          <input
+            className="form-input-num"
+            type="number"
+            autoComplete="off"
+            name="number"
+            min="1"
+            pattern="^[0-9]+$"
+            title="Enter products quantity."
+            value={this.state.number}
+            onChange={this.onInputChange}
+          />
+        </label>
+        <button className="form-btn" type="submit">
+          Add
+        </button>
+      </form>
+    );
+  }
 }
 
 export default AddForm;
--- src/components/ProductsBook/ProductsBook.jsx
+++ src/components/ProductsBook/ProductsBook.jsx
@@ -1,44 +1,46 @@
-import AddForm from "./AddForm/AddForm";
-import ProductList from "./ProductList/ProductList";
+import AddForm from './AddForm/AddForm';
+import ProductList from './ProductList/ProductList';
 
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector, useDispatch } from 'react-redux';
 
-import { addProduct, removeProduct } from "redux/products/prod-slice";
+import { addProduct, removeProduct } from 'redux/products/prod-slice';
 
-import { getProducts } from "redux/products/prod-selectors";
+import { getProducts } from 'redux/products/prod-selectors';
 
-import "../../css/styles.css"
+import '../../css/styles.css';
 
 export default function ProductsBook() {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
-
-    const onAddProduct = (payload) => {
-        const isProduct = products.find(item => item.name.toLowerCase() === payload.name.toLowerCase());
-            if (isProduct) {
-                alert(`${payload.name} is already in list`);
-            } else {
-                dispatch(addProduct(payload))
-            }
+  const onAddProduct = payload => {
+    const isProduct = products.find(
+      item => item.name.toLowerCase() === payload.name.toLowerCase()
+    );
+    if (isProduct) {
+      alert(`${payload.name} is already in list`);
+    } else {
+      dispatch(addProduct(payload));
     }
+  };
 
-    const onRemoveProduct = (payload) => {
-        dispatch(removeProduct(payload))
-    }
+  const onRemoveProduct = payload => {
+    dispatch(removeProduct(payload));
+  };
 
-    const products = useSelector(getProducts)
+  const products = useSelector(getProducts);
 
-    return (
-            <div className="shopping-book">
-                    <h1 className="main-heading">Список покупок</h1>
-                    <AddForm onSubmit={onAddProduct} />
-                    <h2 className="list-heading">Список</h2>
-                    {products.length === 0 ?
-                    <p>Список пустий, додай продукти.</p> : 
-                    <div>
-                        <ProductList products={products} onRemoveProduct={onRemoveProduct} />
-                    </div>
-                    }
-            </div>
-        );
-};
+  return (
+    <div className="shopping-book">
+      <h1 className="main-heading">Shopping list</h1>
+      <AddForm onSubmit={onAddProduct} />
+      <h2 className="list-heading">List</h2>
+      {products.length === 0 ? (
+        <p>List is empty, add something.</p>
+      ) : (
+        <div>
+          <ProductList products={products} onRemoveProduct={onRemoveProduct} />
+        </div>
+      )}
+    </div>
+  );
+}
