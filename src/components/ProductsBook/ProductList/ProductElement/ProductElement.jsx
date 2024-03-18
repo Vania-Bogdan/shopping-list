232bfdc695128626f55ed6e285c922e349b037a8
Vania Bogdan <95138097+Vania-Bogdan@users.noreply.github.com>
2024-03-18 08:57:15 +0100

    TimeOut removed, fast now)

--- src/components/ProductsBook/ProductList/ProductElement/ProductElement.jsx
+++ src/components/ProductsBook/ProductList/ProductElement/ProductElement.jsx
@@ -2,25 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const ProductElement = ({ name, number, id, onRemoveProduct }) => {
-
-  const wait = (ms) => {
-    return new Promise(resolve => {
-      setTimeout(resolve, ms);
-    });
-  };
-
-  async function myFunc(id) {
-    await wait(1000);
-    onRemoveProduct(id)
-  }
-
   return (
     <li key={id} className="product-box">
       <div className='product-content'>
         <p className="product-name">{name}</p>
         <p className="product-name">{number}</p>
       </div>
-      <button className="active" onClick={()=>myFunc(id)}>✓</button>
+      <button className="active" onClick={()=>onRemoveProduct(id)}>✓</button>
     </li>
   );
 };
